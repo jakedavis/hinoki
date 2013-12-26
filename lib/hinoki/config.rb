3204095e0a6258c01940e42a87d39a98c3aff15c
Jake Davis <jake.davis5989@gmail.com>
2013-12-26 21:34:42 +0000

    Re-add config.rb

---
+++ lib/hinoki/config.rb
@@ -0,0 +1,41 @@
+# hinoki/config.rb
+
+require 'json'
+
+class Hinoki
+  class Config
+    attr_accessor(:host, :port)
+
+    def initialize
+      @config = parse_config_file
+      @host = @config['host']
+      @port = @config['port']
+    end
+
+    private
+
+    # Reads the base config file and parses it to a hash
+    def parse_config_file(path=File.join(ENV['HOME'], '.hinoki', 'config'))
+      if !File.exists?(path)
+        puts 'I was unable to find your Hinoki config file!'
+        return defaults 
+      end
+
+      begin 
+        return JSON.parse(File.read(path))
+      rescue JSON::ParserError
+        puts 'I was unable to parse your Hinoki config file!'
+        return defaults
+      end
+    end
+
+    # Default values if the config file cannot be located or read
+    def defaults
+      return {
+        host: 'localhost',
+        port: 4567
+      }
+    end
+
+  end
+end
